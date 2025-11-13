68040abf1b8568c16e2edcf2898aca9211c85b78
Tran Tai Tung <tungbingu58@gmail.com>
2025-11-13 03:24:42 +0000

    Upload New File

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+cd existing_repo
+git remote add origin https://gitlab.com/TranTaiTung/ChronoLeaf.git
+git branch -M main
+git push -uf origin main
+
+public class Class1
+{
+	public Class1()
+	{
+		//test 1
+		1;
+	}
+}
